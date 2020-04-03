a0dd742874e839bfc75e321bc108bf802b24c98b
anezka88 <karamonova.anezka@gmail.com>
2020-04-03 09:50:47 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+int tlacitko = 13;
+int cerpadlo = 15;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(tlacitko, INPUT_PULLUP);
+  pinMode(cerpadlo, OUTPUT);
+}
+
+void loop() {
+  int stav = digitalRead (tlacitko);
+  Serial.println(stav);
+  if (stav == 0)
+  {
+    digitalWrite (cerpadlo, 1);
+  }
+  else
+  {
+    digitalWrite (cerpadlo, 0);
+  }
+  
+}
